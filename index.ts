8b4f8647912ef6d4afa7e9c1a3d7ff69ff90f840
maianh041103 <nguyenmaianh041103@gmail.com>
2023-11-30 03:37:39 +0700

    Lấy ra chủ đề của bài hát

--- index.ts
+++ index.ts
@@ -1,17 +1,28 @@
 import express, { Express, Request, Response } from "express";
 import * as database from "./config/database";
+
+import Topic from "./model/topic.model";
+
+//Nhúng env
 import dotenv from "dotenv";
 dotenv.config();
+//End nhúng env
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
 database.connect();
 
+//Nhúng pug
 app.set('views', './view');
 app.set('view engine', 'pug');
+//End nhúng pug
 
 app.get("/topics", async (req: Request, res: Response): Promise<void> => {
+  const topics = await Topic.find({
+    deleted: false
+  });
+  console.log(topics);
   res.render('client/pages/topics/index.pug');
 });
 
