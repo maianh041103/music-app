ac50481b76ae12f8a70f2b7a91c396a8b6ffa50a
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-02 15:39:47 +0700

    Làm trang chủ đề bài hát

--- controller/client/topic.controller.ts
+++ controller/client/topic.controller.ts
@@ -4,9 +4,11 @@ import Topic from "../../model/topic.model";
 //[GET] /topics
 export const topics = async (req: Request, res: Response): Promise<void> => {
   const topics = await Topic.find({
+    status: "active",
     deleted: false
   });
   res.render('client/pages/topics/index', {
-    pageTitle: "Chủ đề bài hát"
+    pageTitle: "Chủ đề bài hát",
+    topics: topics
   });
 }
