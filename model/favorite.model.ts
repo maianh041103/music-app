dfbb75a0d4b1d06fee63bb7b3377d0f516b58bda
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-05 12:17:34 +0700

    Tao collection Favorite Song

---
+++ model/favorite.model.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const favoriteSongSchema = new mongoose.Schema({
+  songId: String,
+  //userId:String,
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deletedAt: Date
+}, {
+  timestamps: true
+});
+
+const FavoriteSong = mongoose.model("Favorite", favoriteSongSchema, "favorites");
+
+export default FavoriteSong;
