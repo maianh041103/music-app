930ce5364f99ed5d9587653ad01cd2cd469ceafe
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-09 13:41:03 +0700

    Nghe truoc file upload

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -12,3 +12,19 @@ if (uploadImage) {
   });
 }
 // End Upload Image
+
+// Upload audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+  const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+  const uploadAudioPreview = uploadAudio.querySelector("[upload-audio-preview]");
+  uploadAudioInput.addEventListener("change", (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const src = URL.createObjectURL(e.target.files[0]);
+      const audioSource = uploadAudioPreview.querySelector("source");
+      audioSource.src = src;
+      uploadAudioPreview.load();
+    }
+  })
+}
+//End upload audio
